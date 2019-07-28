2ce89601f89f454f92995d644df41770e9059805
Prathmesh <prathmesh.shirgire@gmail.com>
2019-07-28 15:06:38 +0530

    Autocomplete date and time completed..

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.expensetracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,23 +30,85 @@ public class MainActivity extends AppCompatActivity {
     Calendar calendar ;
     SimpleDateFormat simpleDateFormatDate,simpleDateFormatTime  ;
     String timestamp_date,timestamp_time ;
+    TextView txt_date,txt_time;
+    ImageView img_date,img_time;
+
+    final Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         s=(Spinner)findViewById(R.id.spinner_type) ;
-         textViewDate = findViewById(R.id.dateText) ;
-        textViewTime = findViewById(R.id.timeText) ;
+        txt_date = findViewById(R.id.txt_date);
+        img_date = findViewById(R.id.img_date);
+        img_time = findViewById(R.id.img_time);
+        txt_time = findViewById(R.id.txt_time);
         calendar = Calendar.getInstance() ;
+
+        //To set Current Date
         simpleDateFormatDate = new SimpleDateFormat("dd-MM-yyyy") ;
         timestamp_date = simpleDateFormatDate.format(calendar.getTime()) ;
-        textViewDate.setText(timestamp_date);
+        txt_date.setText(timestamp_date);
 
-        simpleDateFormatTime = new SimpleDateFormat("HH:mm:ss") ;
+
+        //To set Current time
+        simpleDateFormatTime = new SimpleDateFormat("HH:mm") ;
         timestamp_time = simpleDateFormatTime.format(calendar.getTime()) ;
-        textViewTime.setText(timestamp_time);
+        txt_time.setText(timestamp_time);
+
+        //Select time on clicking time button
+        img_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        txt_time.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH, i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+                updateLabel();
+            }
+
+
+        };
+
+
+        //Select date on clicking date button
+        img_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        s=(Spinner)findViewById(R.id.spinner_type) ;
+
+
+
+
+
 
         ExpenseHead.add("Select Expense Head");
         ExpenseHead.add("Travel");
@@ -78,6 +145,13 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd-MM-yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        txt_date.setText(sdf.format(myCalendar.getTime()));
     }
 
     private void func_ExpenseType(String state) {
